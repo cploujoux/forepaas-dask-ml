ff29ccd5699bce41fa8a6752a7107728c44e9b9a
2018latracet <thibault.latrace@student-cs.fr>
2022-01-13 16:49:54 +0100

    connect dask worker to postgres db

--- test_scripts/test_postgres.py
+++ test_scripts/test_postgres.py
@@ -1,8 +1,15 @@
-import dask
 import dask.dataframe as dd
-from sqlalchemy import sql
+from distributed import Client
 
 print('connecting')
-df = dd.read_sql_table("consommations","postgresql+psycopg2://root:password@127.0.0.1:5432/test", index_col = 'conso_id');
-print('connected')
+client = Client("tcp://127.0.0.1:8786")
+print("connected")
+
+print("connecting")
+df = dd.read_sql_table(
+    table="consommations",
+    uri="postgresql+psycopg2://root:password@127.0.0.1:5432/test",
+    index_col="conso_id",
+)
+print("connected")
 print(df)
